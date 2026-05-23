3044bdcec5c616c227fd8962129c776a8c94770f
Elev <gustav.k.kri@gmail.com>
2026-05-23 16:31:38 +0200

    Fixed battery percentage when on desktop

--- cpp_files/topMenu.cpp
+++ cpp_files/topMenu.cpp
@@ -3,16 +3,16 @@
 #include "globalSettings.hpp"
 #include "raylib.h"
 #include <fstream>
+#include <filesystem>
 #include <string>
 
 void topMenu() {
-    std::ifstream file("/sys/class/power_supply/BAT0/capacity");
-    int capacity;
-    file >> capacity;
-    file.close();
-    if (capacity < 0 || capacity > 100) {
-        DrawRightText("Connected", windowSize.x - windowSize.x / 30,  windowSize.y / 30, windowSize.y / 40, BLACK);
-    } else {
+    if (std::filesystem::exists("/sys/class/power_supply/BAT0/capacity")) {
+        std::ifstream file("/sys/class/power_supply/BAT0/capacity");
+        int capacity;
+        file >> capacity;
+        file.close();
         DrawRightText(TextFormat("%d\%", capacity), windowSize.x - windowSize.x / 30,  windowSize.y / 30, windowSize.y / 40, BLACK);
     }
 }
+
